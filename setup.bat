dee7117e61326737f336f751358c6f7bf73f46a4
Khanh <khanhtranngoccva@gmail.com>
2024-01-22 02:07:25 +0700

    feat: Setup script

---
+++ setup.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/khanhtranngoccva/miniware-processor.git miniware-processor
+git clone https://github.com/khanhtranngoccva/miniware-frontend.git miniware-frontend
+docker-compose build
